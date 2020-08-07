6621c9f74441c2cddf9cb47ce1ea731ff699d18c
Scott <33047454+ausnorris@users.noreply.github.com>
2020-08-07 16:02:58 +1000

    update to change to locust 1.1

--- weather-test-webonly.py
+++ weather-test-webonly.py
@@ -1,31 +1,11 @@
-from locust import HttpLocust, TaskSet, task
+import random
+from locust import HttpUser, task, between
 
-class UserBehavior(TaskSet):
-    def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """
-        self.login()
+class QuickstartUser(HttpUser):
+    wait_time = between(5, 9)
 
-    def on_stop(self):
-        """ on_stop is called when the TaskSet is stopping """
-        self.logout()
-
-    def login(self):
+    @task
+    def index_page(self):
         self.client.get("/")
-
-
-    @task(2)
-    def index(self):
-        self.client.get("/")
-
-    @task(1)
-    def profile(self):
         self.client.get("/about")
-
-    @task(3)
-    def profile(self):
-        self.client.get("/help")
-
-class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
-    min_wait = 5000
-    max_wait = 9000
+        self.client.get("/weather?address=Canberra")
