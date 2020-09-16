793fc41c4d86b3b0ae6868838d9f759f6dd7c01e
Scott <33047454+ausnorris@users.noreply.github.com>
2020-09-16 14:39:26 +1000

    Update weather-test-webonly.py

--- weather-test-webonly.py
+++ weather-test-webonly.py
@@ -3,10 +3,11 @@ from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     wait_time = between(5, 9)
+    def on_start(self):
+        self.client.verify = False
 
     @task
     def index_page(self):
-        self.client.verify = False
         self.client.get("/", verify=False)
         self.client.get("/about", verify=False)
         self.client.get("/weather/Canberra", verify=False)
